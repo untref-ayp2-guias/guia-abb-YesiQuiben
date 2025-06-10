866eba01427a0e24b91d6b50450f66d9fcf937fc
YesiQuiben <ylquiben@gmail.com>
2025-06-09 23:18:51 -0300

    ok

--- ejercicios/ej01_test.go
+++ ejercicios/ej01_test.go
@@ -31,10 +31,51 @@ func TestSecLargeElem(t *testing.T) {
 	p, _ := SecondLargestElement(bst)
 	assert.Equal(t, 20, p)
 }
+func TestSecLargeElemIs25(t *testing.T) {
+	bst := binarytree.NewBinarySearchTree[int]()
+	bst.Insert(15)
+	bst.Insert(10)
+	bst.Insert(20)
+	bst.Insert(8)
+	bst.Insert(12)
+	bst.Insert(16)
+	bst.Insert(25)
+	bst.Insert(30)
+	p, _ := SecondLargestElement(bst)
+	assert.Equal(t, 25, p)
+}
+
+func TestSecLargeElemTwoNodesIs5(t *testing.T) {
+	bst := binarytree.NewBinarySearchTree[int]()
+	bst.Insert(10)
+	bst.Insert(5)
+	p, _ := SecondLargestElement(bst)
+	assert.Equal(t, 5, p)
+}
+
+func TestSecLargeElemRightDeepIs20(t *testing.T) {
+	bst := binarytree.NewBinarySearchTree[int]()
+	bst.Insert(10)
+	bst.Insert(5)
+	bst.Insert(20)
+	bst.Insert(30)
+	p, _ := SecondLargestElement(bst)
+	assert.Equal(t, 20, p)
+}
+
+func TestSecLargeElemLeftSubtreeOfMaxIs19(t *testing.T) {
+	bst := binarytree.NewBinarySearchTree[int]()
+	bst.Insert(10)
+	bst.Insert(5)
+	bst.Insert(20)
+	bst.Insert(18)
+	bst.Insert(19)
+	p, _ := SecondLargestElement(bst)
+	assert.Equal(t, 19, p)
+}
 
 func TestNilSecLargeElem(t *testing.T) {
 	bst := binarytree.NewBinarySearchTree[int]()
-
 	_, err := SecondLargestElement(bst)
 	assert.EqualError(t, err, "No hay valores")
 }
